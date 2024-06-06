2469da7d5392b771f0d2907ed3ae4039277e7963
Trantuan2306 <trantuan230600@gmail.com>
2024-06-06 17:25:31 +0700

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,8 +3,12 @@ dotenv.config();
 
 const configCors = (app) => {
   app.use(function(req, res, next) {
+    const allowedOrigins = [process.env.PORT_URL, 'http://localhost:3001']; // Thêm URL cần cho phép gọi API vào đây
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-    res.setHeader("Access-Control-Allow-Origin", process.env.PORT_URL);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
